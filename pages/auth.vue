df7ad1a12420d7f8d6fdd0a1e856a6f60a9a3f03
Abdullox Saidmurodov <a.saidmurodov@mail.ru>
2025-02-04 11:43:17 +0500

    2

---
+++ pages/auth.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+    definePageMeta({
+        layout: 'auth',
+    })
+</script>
+
+<template>
+    <div>
+        <h1>Auth page</h1>
+        <SharedButton>Button</SharedButton>
+    </div>
+</template>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    // const doc = "Documentation"
+    const route = useRoute()
+</script>
+
+<template>
+    <div>
+        <!-- <h1>Post id {{ doc }}</h1> -->
+        <h1>Documentation id: {{ route.params.id }}</h1>
+    </div>
+</template>
